bb9e8f0ad1cf53c9ddefa66819b2dc7b1e30a823
Stas <morstas99@mail.ru>
2022-02-05 18:20:32 +0300

    check InputWindows

--- Sendbox/src/SendboxApp.cpp
+++ Sendbox/src/SendboxApp.cpp
@@ -5,7 +5,7 @@ public:
 	
 	void OnUpdate() override { 
 		if (Sas::Input::IsKeyPressed(Sas::Key::Tab))
-			SS_CORE_TRACE("Key Tab Press");
+			SS_CORE_TRACE("Key Tab Press(pull)");
 	}
 
 	void OnEvent(Sas::Event& e) override {
@@ -13,6 +13,20 @@ public:
 		if (e.GetEventType() == Sas::EventType::KeyPressed) {
 			Sas::KeyPressedEvent& event = (Sas::KeyPressedEvent&)e;
 			SS_CORE_TRACE("{0}", (char)event.GetKeyKode());
+			
+
+			if (Sas::Input::IsKeyPressed(Sas::Key::RightAlt)) {
+				SS_CORE_TRACE("Key Right alt Press(event)");
+
+			}
+			else if (Sas::Input::IsKeyPressed(Sas::Key::LeftAlt)) {
+				SS_CORE_TRACE("Key Left alt Press(event)");
+
+			}
+			else if (Sas::Input::IsKeyPressed(Sas::Key::Tab)) {
+				SS_CORE_TRACE("Key Tab Press(event)");
+			}
+			else SS_CORE_TRACE("{0}", (char)event.GetKeyKode());
 		}
 	};
 };
